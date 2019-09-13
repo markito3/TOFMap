b6f0d52ed584baada3a10fd33368a086e1977ab2
markito3 <marki@jlab.org>
2019-09-13 15:15:02 -0400

    populate moduleLocation table

--- do_it.sh
+++ do_it.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mysql -utofuser -e 'drop database if exists TOFComponent'
 mysql -utofuser -e 'create database TOFComponent'
-cat pmt.sql pmt_location.sql | mysql -utofuser TOFComponent
+cat pmt.sql pmt_location.sql module_location.sql | mysql -utofuser TOFComponent
 working_dir=`pwd`
 mysql -utofuser TOFComponent -e "load data infile '$working_dir/rachel.txt' into table TOFComponent.pmt"
 rm -f pmt_location.txt
 ./pmt_location.py > pmt_location.txt
 mysql -utofuser TOFComponent -e "load data infile '$working_dir/pmt_location.txt' into table TOFComponent.pmtLocation"
 awk '{print "UPDATE pmt SET pmtLocationId = (SELECT id FROM pmtLocation WHERE label=\""$3"\") WHERE serialNo = \""$2"\";"}' < rachel_label.txt | mysql -utofuser TOFComponent
-
+awk '{print "INSERT INTO moduleLocation set", "id="$1",", "label=\""$2"\",", "layer="$3",", "position="$4",", "moduleType=\""$5"\",", "end="$6",", "orientation=\""$7"\";"}' < module_location.txt | mysql -utofuser TOFComponent
---
+++ module_location.sql
@@ -0,0 +1,11 @@
+create table moduleLocation (
+  id smallint not null,
+  layer smallint not null,
+  position smallint not null,
+  moduleType enum('standard', 'narrow', 'short') not null,
+  end smallint default null,
+  orientation enum('up', 'down') not null,
+  label varchar(16) not null,
+  serialNo varchar(64) default null,
+  primary key (id)
+);
