5b7067acc5650f6b37b0a8cc65f44820f5da6ebd
Mark Ito <marki@jlab.org>
2019-09-15 20:31:20 -0400

    Add output to indicate necessary GRANTs for the database access.

--- do_it.sh
+++ do_it.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-mysql -utofuser -e 'drop database if exists TOFComponent'
-mysql -utofuser -e 'create database TOFComponent'
-cat pmt.sql pmt_location.sql module_location.sql | mysql -utofuser TOFComponent
+echo necessary grants are:
+echo \ \ CREATE USER \'tofuser\'@\'hostofchoice.domainofchoice\'\;
+echo \ \ GRANT ALL on TOFMap.\* to \'tofuser\'@\'hostofchoice.domainofchoice\'\;
+echo \ \ GRANT FILE ON \*.\* TO \'tofuser\'@\'hostofchoice.domainofchoice\'\;
+mysql -utofuser -e 'drop database if exists TOFMap'
+mysql -utofuser -e 'create database TOFMap'
+cat pmt.sql pmt_location.sql module_location.sql | mysql -utofuser TOFMap
 working_dir=`pwd`
-mysql -utofuser TOFComponent -e "load data infile '$working_dir/rachel.txt' into table TOFComponent.pmt"
+mysql -utofuser TOFMap -e "load data local infile '$working_dir/rachel.txt' into table TOFMap.pmt"
 rm -f pmt_location.txt
 ./pmt_location.py > pmt_location.txt
-mysql -utofuser TOFComponent -e "load data infile '$working_dir/pmt_location.txt' into table TOFComponent.pmtLocation"
-awk '{print "UPDATE pmt SET pmtLocationId = (SELECT id FROM pmtLocation WHERE label=\""$3"\") WHERE serialNo = \""$2"\";"}' < rachel_label.txt | mysql -utofuser TOFComponent
-awk '{print "INSERT INTO moduleLocation set", "id="$1",", "label=\""$2"\",", "layer="$3",", "position="$4",", "moduleType=\""$5"\",", "end="$6",", "orientation=\""$7"\";"}' < module_location.txt | mysql -utofuser TOFComponent
-./mod_pmt_locations.py | awk '{print "UPDATE pmtLocation set moduleLocationId = (SELECT id from moduleLocation where label = \""$2"\") where label = \""$1"\";"}' | mysql -utofuser TOFComponent
+mysql -utofuser TOFMap -e "load data local infile '$working_dir/pmt_location.txt' into table TOFMap.pmtLocation"
+awk '{print "UPDATE pmt SET pmtLocationId = (SELECT id FROM pmtLocation WHERE label=\""$3"\") WHERE serialNo = \""$2"\";"}' < rachel_label.txt | mysql -utofuser TOFMap
+awk '{print "INSERT INTO moduleLocation set", "id="$1",", "label=\""$2"\",", "layer="$3",", "position="$4",", "moduleType=\""$5"\",", "end="$6",", "orientation=\""$7"\";"}' < module_location.txt | mysql -utofuser TOFMap
+./mod_pmt_locations.py | awk '{print "UPDATE pmtLocation set moduleLocationId = (SELECT id from moduleLocation where label = \""$2"\") where label = \""$1"\";"}' | mysql -utofuser TOFMap
