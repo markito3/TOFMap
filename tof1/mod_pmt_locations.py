b0a0e8e04c00460c1bdbf8f43b8d34a367663815
markito3 <marki@jlab.org>
2019-09-27 13:37:49 -0400

    Add leading zeros to module labels. Fix nominal web page.

--- tof1/mod_pmt_locations.py
+++ tof1/mod_pmt_locations.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # looping over PMT labels, calculate module label
+def pad(i):
+    out = '%02d' % i
+    return out
 for i in range(1, 22):
-    print("TOF-UP-" + str(45 - i) + " X-" + str(i))
-    print("TOF-DW-" + str(45 - i) + " X-" + str(i))
-    print("TOF-N-" + str(i) + " Y-" + str(i))
-    print("TOF-S-" + str(i) + " Y-" + str(i))
+    print("TOF-UP-" + str(45 - i) + " X-" + pad(i))
+    print("TOF-DW-" + str(45 - i) + " X-" + pad(i))
+    print("TOF-N-" + str(i) + " Y-" + pad(i))
+    print("TOF-S-" + str(i) + " Y-" + pad(i))
 for i in range(22, 24):
-    print("TOF-UP-" + str(45 - i) + " X-" + str(i + 2))
-    print("TOF-DW-" + str(45 - i) + " X-" + str(i))
-    print("TOF-N-" + str(i) + " Y-" + str(i))
-    print("TOF-S-" + str(i) + " Y-" + str(i + 2))
+    print("TOF-UP-" + str(45 - i) + " X-" + pad(i + 2))
+    print("TOF-DW-" + str(45 - i) + " X-" + pad(i))
+    print("TOF-N-" + str(i) + " Y-" + pad(i))
+    print("TOF-S-" + str(i) + " Y-" + pad(i + 2))
 for i in range(24, 45):
-    print("TOF-UP-" + str(45 - i) + " X-" + str(i + 2))
-    print("TOF-DW-" + str(45 - i) + " X-" + str(i + 2))
-    print("TOF-N-" + str(i) + " Y-" + str(i + 2))
-    print("TOF-S-" + str(i) + " Y-" + str(i + 2))
+    print("TOF-UP-" + str(45 - i) + " X-" + pad(i + 2))
+    print("TOF-DW-" + str(45 - i) + " X-" + pad(i + 2))
+    print("TOF-N-" + str(i) + " Y-" + pad(i + 2))
+    print("TOF-S-" + str(i) + " Y-" + pad(i + 2))
--- tof1/nominal_report.sql
+++ tof1/nominal_report.sql
@@ -1,4 +1,4 @@
-SELECT pmtLocation.id as ID, pmtLocation.label AS Cable_Label, serialNo as PMT_Serial_No, layer as Layer, moduleType as Module_Type
+SELECT pmtLocation.id as ID, pmtLocation.label AS Cable_Label, serialNo as PMT_Serial_No, layer as Layer, moduleLocation.label as Module_Label, moduleType as Module_Type, pmtLocation.end as Module_End
   FROM pmtLocation, pmt, moduleLocation
   WHERE pmt.pmtLocationId = pmtLocation.id
   AND moduleLocationId = moduleLocation.id ORDER BY pmtLocation.id;
