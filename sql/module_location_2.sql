422894be8dd3b3df81f044f6f5897117b860cb8e
Mark Ito <marki@jlab.org>
2019-09-28 13:38:13 -0400

    Add comments with descriptions of the columns.

--- sql/module_location_2.sql
+++ sql/module_location_2.sql
@@ -1,3 +1,16 @@
+--
+-- moduleLocation table
+--  for TOF II
+--
+-- id: identifier
+-- layer: upstream (-1) or downstream (+1)
+-- moduleType: one of seven geometries for the modules
+-- end: NULL for double ended modules, -1 or +1 for single-ended, sign goes
+--      with physicist coordinate system
+-- orientation: tilt of light guides, up or down, up = tilted upstream
+-- label: text label for the module location
+--
+
 create table moduleLocation (
   id smallint not null,
   layer smallint not null,
--- sql/pmt_location.sql
+++ sql/pmt_location.sql
@@ -1,3 +1,12 @@
+--
+-- pmtLocation table
+--
+-- id: identifier
+-- moduleLocationId: pointer to the location of a module
+-- end: end of module that is occupied by this pmt
+-- label: label of this pmt location, use the cable label from Fernando
+--
+
 create table pmtLocation (
   id smallint not null,
   moduleLocationId smallint default 0 not null,
