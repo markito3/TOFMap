06a28caf37b6d94f91cb58dd70dee429a4d1ef1f
Mark Ito <marki@jlab.org>
2019-09-28 15:28:12 -0400

    Creates a complete database, putatively.

--- tof2/do_it.sh
+++ tof2/do_it.sh
@@ -7,9 +7,9 @@ mysql -utofuser -e 'create database TOFMap2'
 cat ../sql/pmt.sql ../sql/pmt_location.sql ../sql/module_location_2.sql \
     | mysql -utofuser TOFMap2
 working_dir=`pwd`
-grep -v \# pmt_serial.txt \
+grep -v \# pmt_info.txt \
     | awk '{print "INSERT INTO pmt set serialNo = \""$2"\", type = \""$3"\", pmtLocationId = "$1";"}' \
     | mysql -utofuser TOFMap2
 awk '{print "INSERT INTO moduleLocation set", "id="$1",", "label=\""$2"\",", "layer="$3",", "moduleType=\""$4"\",", "end="$5",", "orientation=\""$6"\";"}' \
-    < module_location.txt | mysql -utofuser TOFMap2
-#./mod_pmt_locations.py | awk '{print "UPDATE pmtLocation set moduleLocationId = (SELECT id from moduleLocation where label = \""$2"\") where label = \""$1"\";"}' | mysql -utofuser TOFMap2
+    < module_info.txt | mysql -utofuser TOFMap2
+grep -v \# pmt_info.txt | awk '{print "INSERT INTO pmtLocation SET id = "$1", moduleLocationId = (SELECT id FROM moduleLocation WHERE label = \""$6"\"), end = "$7", label = \""$5"\";"}' | mysql -utofuser TOFMap2
