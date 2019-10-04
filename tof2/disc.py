7e2dec84ffbf4c338adacbe0f82c85666148ada3
markito3 <marki@jlab.org>
2019-10-04 18:16:21 -0400

    Start on discriminator stuff.

---
+++ tof2/disc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+i = 0
+cable_id = 0
+for line in sys.stdin:
+    i = i + 1
+    liner = line.rstrip()
+#    print(liner)
+    if i == 1:
+        slots = liner.split(',')
+        nslots = len(slots) - 1
+        for j in range(1, nslots + 1):
+            print("INSERT INTO discLocation SET id = " + str(j) + ", slot = " + slots[j] + ";")
+    elif i == 2:
+        serialNos = liner.split(',')
+        for j in range(1, nslots + 1):
+            print("INSERT INTO disc SET serialNo = \"" + serialNos[j] + "\", discLocationId = " + str(j) + ";")
+    else:
+        cables = liner.split(',')
+        channel = cables[0]
+        for j in range(1, nslots + 1):
+            label = cables[j]
+            if label[0:4] == 'TOF-':
+                cable_id = cable_id + 1
+                print("INSERT INTO discCable SET id = " + str(cable_id) + ", label = \"" + label + "\", discLocationId = " + str(j) + ", channel = \"" + channel + "\", pmtLocationId = (SELECT id FROM pmtLocation WHERE label = \"" + label + "\");")
