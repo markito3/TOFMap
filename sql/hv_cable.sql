270c42b30d222115e28f1671d3cadd19ffc78f11
markito3 <marki@jlab.org>
2019-10-04 08:18:12 -0400

    Add hv cable definition and hv channel map.

---
+++ sql/hv_cable.sql
@@ -0,0 +1,17 @@
+--
+-- hvCable table
+--
+-- id: identifier
+-- label: label of this cable, from Fernando
+-- pmtLocationId: pmt on one end
+-- hvCardLocationId: HV card in HV crate
+-- channel: channel on card
+--
+
+create table hvCable (
+  id smallint not null,
+  label varchar(128),
+  pmtLocationId smallint,
+  hvCardLocationId smallint,
+  primary key (id)
+);
